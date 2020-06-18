801fd1603b8d03657e92b325981cdfc8eac69a0a
smeleshkina <sdudich@yandex.ru>
2020-06-18 14:24:54 +0300

    ZigZag added

---
+++ CodingProblemsLib/Problems/ZigZag.cs
@@ -0,0 +1,70 @@
+﻿/*The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+   P   A   H   N
+   A P L S I I G
+   Y   I   R
+   And then read line by line: "PAHNAPLSIIGYIR"
+
+   Write the code that will take a string and make this conversion given a number of rows:
+
+   string convert(string s, int numRows);
+   Example 1:
+
+   Input: s = "PAYPALISHIRING", numRows = 3
+   Output: "PAHNAPLSIIGYIR"
+   Example 2:
+
+   Input: s = "PAYPALISHIRING", numRows = 4
+   Output: "PINALSIGYAHRPI"
+   Explanation:
+
+   P     I    N
+   A   L S  I G
+   Y A   H R
+   P     I*/
+
+using System.Linq;
+
+namespace CodingProblemsLib.Problems
+{
+    public class ZigZag
+    {
+        public static string Convert(string s, int numRows)
+        {
+            int patternLen = numRows > 1 ? numRows * 2 - 2 : 1;
+
+            int[] indices = new int[s.Length];
+            int idx = 0;
+            for (int row = 0; row < numRows; ++row)
+            {
+                if (row == 0 || row == numRows - 1)
+                {
+                    int currIdx = row;
+                    while (currIdx < s.Length)
+                    {
+                        indices[idx] = currIdx;
+                        ++idx;
+                        currIdx += patternLen;
+                    }
+
+                    continue;
+                }
+
+                int currentIdx = row;
+                int idx1 = 0;
+                int firstDist = patternLen - 2 * row;
+                int secondDist = patternLen - firstDist;
+                while (currentIdx < s.Length)
+                {
+                    indices[idx] = currentIdx;
+                    ++idx;
+
+                    currentIdx += idx1 % 2 == 0 ? firstDist : secondDist;
+                    ++idx1;
+                }
+            }
+
+            return string.Join("", indices.Select(i => s[i]));
+        }
+    }
+}
---
+++ CodingProblemsTests/ZigZagTest.cs
@@ -0,0 +1,23 @@
+﻿using CodingProblemsLib.Problems;
+using NUnit.Framework;
+
+namespace CodingProblemsTests
+{
+    public class ZigZagTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+        [TestCase("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+        [TestCase("A", 1, "A")]
+        [TestCase("PAYPALISHIRING", 1, "PAYPALISHIRING")]
+        public void Test(string s, int rows, string result)
+        {
+            Assert.AreEqual(ZigZag.Convert(s, rows), result);
+        }
+    }
+}
