65e44331d36de0c6e452efa2ad482ef24b97ae49
Svetlana Meleshkina <sdudich@yandex.ru>
2021-07-26 14:06:14 +0300

    FindAndReplaceInString added

---
+++ CodingProblemsLib/Problems/FindAndReplaceInString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace CodingProblemsLib.Problems
+{
+    public class FindAndReplaceInString
+    {
+        public static string Run(string s, int[] indices, string[] sources, string[] targets)
+        {
+            int idx = 0;
+            StringBuilder sb = new StringBuilder();
+            while (idx < s.Length)
+            {
+                int replaceIdx = Array.IndexOf(indices, idx);
+                if (replaceIdx == -1)
+                {
+                    sb.Append(s[idx++]);
+                    continue;
+                }
+
+                int len = sources[replaceIdx].Length;
+                if (0 == string.CompareOrdinal(s.Substring(idx, len), sources[replaceIdx]))
+                {
+                    sb.Append(targets[replaceIdx]);
+                    idx += len;
+                }
+                else
+                {
+                    sb.Append(s[idx++]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CodingProblemsTests/FindAndReplaceStringTests.cs
@@ -0,0 +1,15 @@
+﻿using CodingProblemsLib.Problems;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class FindAndReplaceStringTests
+    {
+        [Theory]
+        [InlineData("abcd", new [] { 0, 2}, new [] { "a", "cd"}, new [] {"eee", "ffff"}, "eeebffff")]
+        public void Test(string s, int[] indices, string[] sources, string[] targets, string result)
+        {
+            Assert.Equal(FindAndReplaceInString.Run(s, indices, sources, targets), result);
+        }
+    }
+}
